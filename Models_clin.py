57b173c7a2b1dd17433a38bef5f6df6268583c54
DrRadan <rodoniki@gmail.com>
2017-11-02 17:51:28 -0400

    anchor tag test

--- Models_clin.py
+++ Models_clin.py
@@ -8,7 +8,6 @@ clin_name = input("What is the name of the csv file holding the clinical data? \
 regressor = input("What is the name of the column in your clinical dataset {} that holds the values of the risk factor (regressor)?".format(clin_name))
 pct_train = float(input("What fraction of the clinical data do you want to use for training? (Recommended is 0.7) "))
 
-# Linear model (reference) test
 print('##############	Calculating Linear Regression Model ########################')
 bar1 = ProgressBar()
 AUC_lm = []
@@ -20,12 +19,9 @@ for i in bar1(range(50)):
     AUC_lm.append(empirical_auc(data,Y_hat_lm))
 print("The AUC for the LM is {:.2f}, stdev = {:.2f}.".format(np.average(AUC_lm),np.std(AUC_lm)))
 
-#Random forest test
 results = {key: [] for key in range(4,51,2)}
 bar2 = ProgressBar()
-bar3 = ProgressBar()
 print('##############	Calculating Random Forest Model ########################')
-#Identify optimal parameters
 for j in bar2(range(50)):
     for i in range(4,51,2):
         forest = RandomForestClassifier(n_estimators = 200, min_samples_leaf = 0.05,  max_depth=i)
@@ -35,6 +31,16 @@ for j in bar2(range(50)):
         results[i].append(auc) 
 for i in range(4,51,2):
     print("Tree depth of {} has average AUC of {:.2f} with stdev {:.2f}".format(i, np.average(results[i]), np.std(results[i])))
+
+answer = "no"
+while answer == "no":
+    mod = input("\n\nDo you want to proceed with the linear regression model (LR) or random forest (RF) model?").lower()
+    if mod == "lr":
+        e_lm = np.subtract(data['test_Y'], Y_hat_lm)
+        oe_lm = data['test_Y']/ Y_hat_lm
+    if mod == "rf":
+    else:
+        answer = input("I am expecting either LR (linear regression) or RF (random forest). You entered {}.\n Do you want to quit? (Yes/No)". format(mod)).lower()
 
 
 
@@ -49,8 +55,7 @@ Y_hat_rf = forest.predict(data['test_X'])
 
 
 
-#e_lm = np.subtract(data['test_Y'], Y_hat_lm)
-#oe_lm = data['test_Y']/ Y_hat_lm   
+
 
 #e_rf = np.subtract(data['test_Y'], Y_hat_lm)
 #oe_rf = data['test_Y']/ Y_hat_lm	
